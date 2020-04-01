e1c75b4434d8983cdb92fe7f127688b0b2933c26
adryevov <a.dryevov@gmail.com>
2020-04-01 05:35:28 -0400

    upload week9 lab

---
+++ wk12/csfr.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<!--[if lt IE 7]>      <html class="no-js lt-ie9 lt-ie8 lt-ie7"> <![endif]-->
+<!--[if IE 7]>         <html class="no-js lt-ie9 lt-ie8"> <![endif]-->
+<!--[if IE 8]>         <html class="no-js lt-ie9"> <![endif]-->
+<!--[if gt IE 8]><!--> <html class="no-js"> <!--<![endif]-->
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <title></title>
+        <meta name="description" content="">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel="stylesheet" href="">
+    </head>
+    <body>
+
+    <form action="/action_page.php" method="post">
+        <label for="username">Username:</label><br>
+        <input type="text" id="username" name="username" value=""><br>
+        <label for="password">Password:</label><br>
+        <input type="text" id="password" name="password" value=""><br><br>
+        <input type="submit" value="Submit">
+    </form> 
+    <div>
+        
+    </div>
+    
+    
+
+
+        <!--[if lt IE 7]>
+            <p class="browsehappy">You are using an <strong>outdated</strong> browser. Please <a href="#">upgrade your browser</a> to improve your experience.</p>
+        <![endif]-->
+        
+        <script src="" async defer></script>
+    </body>
+</html>
---
+++ wk9/test.php
@@ -0,0 +1,19 @@
+
+<h1>Weak Password</h1>
+    <form method="post" action="<?php echo $_SERVER['PHP_SELF']; ?>">
+        <label>Password</label>
+        <input type="hidden" id="username" name="username" value="CrimsonTyphoon1999">
+        <input type="password" name="password">
+        <br/>
+        <input type="submit">
+</form>
+
+<?php
+    $common_passwords = array("123456", "123456789", "qwerty", "password", "111111", "12345678", "abc123" , "1234567", "password1", "12345");
+    if(isset($_POST['submit'])){
+            if(in_array($_POST['password'], $common_passwords)){
+                ob_end_clean();
+                echo "<h2> Welcome ". $_POST['username']. "to your Portal! </h2>";
+            }
+    };
+?>
